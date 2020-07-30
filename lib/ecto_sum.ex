bd743803d60a76277db0800631dfda2f9585aee9
croixxant <croixxxant@gmail.com>
2020-07-31 02:01:48 +0900

    :seedling: Initial commit

---
+++ lib/ecto_sum.ex
@@ -0,0 +1,18 @@
+defmodule EctoSum do
+  @moduledoc """
+  Documentation for `EctoSum`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoSum.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_sum_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoSumTest do
+  use ExUnit.Case
+  doctest EctoSum
+
+  test "greets the world" do
+    assert EctoSum.hello() == :world
+  end
+end
